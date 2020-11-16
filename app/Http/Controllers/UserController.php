5b19774de47c4858b1099b1958c34e9110bb4766
Henry Abayan <henry.abayan@gmail.com>
2020-11-16 21:05:48 +0800

    login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use App\Models\User;
+class UserController extends Controller
+{
+    public function login(Request $request){
+    	$user = User::where(['email' => $request->email])->first();
+    	
+    	if(!$user || !Hash::check($request->password, $user->password)){
+    		return 'not match';
+    	}else{
+    		$request->session()->put('user', $user);
+    		return redirect('/');
+
+    	}
+    }
+}
